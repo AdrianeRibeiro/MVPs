91ecec8403ae64c7d95e8c3d8fb56cff4653fc90
Adriane Ribeiro <adrianeribeiro94@gmail.com>
2022-03-12 16:51:05 -0300

    preparando ambiente

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import Head from 'next/head'
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <div className="grid grid-cols-2 gap-4">
+      <div>red</div>
+      <div>blue</div>
+    </div>
+  )
+}
