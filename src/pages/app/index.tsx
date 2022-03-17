eb8352b8b63a5f1570cb8348dfd46fa2b2d4813e
Adriane Ribeiro <adrianeribeiro94@gmail.com>
2022-03-17 08:04:47 -0300

    adicionando prisma

--- src/pages/app/index.tsx
+++ src/pages/app/index.tsx
@@ -1,10 +1,64 @@
+//import { Tas } from '@prisma/client'
 import { signOut } from "next-auth/react";
+import { GetServerSideProps  } from "next";
+import { prisma } from "../../lib/prisma";
+import { FormEvent, useState } from "react";
 
-export default function App() {
+type TasksProps = {
+  //tasks: Tasks
+  id: string
+  title: string
+  isDone: boolean
+  date: string
+}
+
+export default function App({ data }: any) {
+  const tasks = data
+  const [newTask, setNewTask] = useState('')
+
+  async function handleCreateTask(event: FormEvent) {
+    event.preventDefault()
+
+    await fetch('http://localhost:3000/api/tasks/create', {
+      method: 'POST',
+      body: JSON.stringify({ title: newTask}),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+  }
+
   return (
     <div>
-      <h1>Hello World</h1>
       <button onClick={() => signOut()}>Sair</button>
+      <ul>
+        {
+          tasks?.map(task => <li key={task.id}>{task.title}</li>)
+        }
+      </ul>
+      <form onSubmit={handleCreateTask}>
+        <input type="text" value={newTask} onChange={e => setNewTask(e.target.value)}/>
+        <button type="submit">Cadastrar</button>
+      </form>
     </div>
   )
+}
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const tasks = await prisma.task.findMany()
+
+  const data = tasks.map(task => {
+    return {
+      id: task.id,
+      title: task.title,
+      isDone: task.isDone,
+      date: task.createdAt.toISOString()
+    }
+  })
+
+  return {
+    props: {
+      data
+    }
+  }
 }
